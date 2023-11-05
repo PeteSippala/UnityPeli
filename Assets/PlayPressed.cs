089f3919f2366b2f3fd91c658bb66865830b938c
PeteSippala <t1sipe00@students.oamk.fi>
2023-11-05 13:38:59 +0200

    settings added

---
+++ Assets/PlayPressed.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayPressed : MonoBehaviour
+{
+    
+     public void OnButtonClicked()
+    {
+        SceneManager.LoadScene("Scenes/SampleScene");
+    }
+
+}
